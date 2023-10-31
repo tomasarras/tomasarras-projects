a513b4bda8d9ab94bc1aa2dc5b733eff37acf4e4
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2023-10-31 02:31:57 -0300

    BTTFCircuits

--- components/BTTFCircuits/index.js
+++ components/BTTFCircuits/index.js
@@ -13,10 +13,41 @@ export default function BTTFCircuits({ destination, present, lastTime }) {
   };
   const [panel, setPanel] = useState({ destination: empty, present: empty, lastTime: empty });
   const [remainingMode, setRemainingMode] = useState(false);
+  const switchRemainingMode = () => setRemainingMode(!remainingMode);
+
+  const calcRemainingTime = date => {
+    const diff = date - new Date();
+    const millisecondsInYear = 365 * 24 * 60 * 60 * 1000;
+    const millisecondsInDay = 24 * 60 * 60 * 1000;
+    const millisecondsInMin = 60 * 1000;
+
+    const year = Math.floor(diff / millisecondsInYear)
+      .toString()
+      .padStart(4, '0');
+    const month = "---";
+    const day = Math.min(Math.floor(diff / millisecondsInDay) % 365, 99)
+      .toString()
+      .padStart(2, '0');
+    const hour = Math.floor((diff / (60 * 60 * 1000)) % 24)
+      .toString()
+      .padStart(2, '0');
+    const min = Math.floor((diff / millisecondsInMin) % 60)
+      .toString()
+      .padStart(2, '0');
+    return {
+      month,
+      day,
+      year,
+      hour,
+      min,
+      isAm: false,
+      isPm: false,
+    }
+  };
 
   const updatePanel = () => {
     setPanel({
-      destination: dateDetails(destination),
+      destination: remainingMode ? calcRemainingTime(destination) : dateDetails(destination),
       present: dateDetails(present == undefined ? new Date() : present),
       lastTime: dateDetails(lastTime),
     });
@@ -24,12 +55,12 @@ export default function BTTFCircuits({ destination, present, lastTime }) {
 
   useEffect(() => {
     updatePanel();
-    const interval = setInterval(updatePanel, 3000);
+    const interval = setInterval(updatePanel, 1000);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [remainingMode]);
 
   const dateDetails = date => {
     if (date === undefined)
@@ -52,7 +83,7 @@ export default function BTTFCircuits({ destination, present, lastTime }) {
 
   return (<>
     <div className={s[`overall-container`]}>
-      <div className={`${s[`individual-time-circuit-wrapper-area`]} ${s[`individual-time-circuit-wrapper-area`]} ${s[`destination`]}`}>
+      <div onClick={switchRemainingMode} className={`${s[`individual-time-circuit-wrapper-area`]} ${s[`individual-time-circuit-wrapper-area`]} ${s[`destination`]}`}>
         <div className={`${s["individual-time-circuit-content"]}`}>
 
           <div className={`${s["row-of-labels"]}`}>
@@ -67,9 +98,9 @@ export default function BTTFCircuits({ destination, present, lastTime }) {
             <div className={`${s["console-display"]} ${s["lcd"]} ${s["console-month"]}`}><div className={`${s["faded-eights"]}`}>888</div>{panel.destination.month}</div>
             <div className={`${s["console-display"]} ${s["lcd"]} ${s["console-day"]}`}><div className={`${s["faded-eights"]}`}>88</div>{panel.destination.day}</div>
             <div className={`${s["console-display"]} ${s["lcd"]} ${s["console-year"]}`}><div className={`${s["faded-eights"]}`}>8888</div>{panel.destination.year}</div><div className={`${s["console-display"]} ${s["console-am-pm"]}`}>&nbsp;<div className={`${s["am-label"]}`}>AM</div>
-              <div className={`${s["indicator-light"]} ${s["am"]} ${panel.destination.isAm ? s["light-on"] : s["light-off"]}`}>&nbsp;</div>
+              <div className={`${s["indicator-light"]} ${s["am"]} ${remainingMode ? s["light-red"] : panel.destination.isAm ? s["light-on"] : s["light-off"]}`}>&nbsp;</div>
               <div className={`${s["pm-label"]}`}>PM</div>
-              <div className={`${s["indicator-light"]} ${s["pm"]} ${panel.destination.isPm ? s["light-on"] : s["light-off"]}`}>&nbsp;</div>
+              <div className={`${s["indicator-light"]} ${s["pm"]} ${remainingMode ? s["light-red"] : panel.destination.isPm ? s["light-on"] : s["light-off"]}`}>&nbsp;</div>
             </div><div className={`${s["console-display"]} ${s["lcd"]} ${s["console-hour"]}`}><div className={`${s["faded-eights"]}`}>88</div>{panel.destination.hour}</div>
 
             <div className={`${s["console-display"]} ${s["console-colon-lights"]}`}>
