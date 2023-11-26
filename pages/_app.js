69e4a68917b48bdc6026d0e702bbfb5d610c5b0a
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2023-11-26 16:16:51 -0300

    chore: googlemaps and fingerprint removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
-import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react';
-import { Wrapper } from "@googlemaps/react-wrapper";
+
 
 function MyApp({ Component, pageProps }) {
 
-  return process.env.NEXT_PUBLIC_FINGERPRINTJS_PUBLIC_KEY !== undefined ? (<FpjsProvider loadOptions={{
-    apiKey: process.env.NEXT_PUBLIC_FINGERPRINTJS_PUBLIC_KEY
-  }}>
-    <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_PUBLIC_KEY}>
-      <Component {...pageProps} />
-    </Wrapper>
-  </FpjsProvider>)
-  : (<Component {...pageProps} />)
+  return (<Component {...pageProps} />);
 }
 
 export default MyApp;
