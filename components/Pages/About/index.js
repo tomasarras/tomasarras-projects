cb576b9a6ee35ef42712f7c6935e6fde82c6d493
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2023-12-26 21:27:52 -0300

    styles: About view

--- components/Pages/About/index.js
+++ components/Pages/About/index.js
@@ -3,13 +3,14 @@ import DesktopAnimation from '../../DesktopAnimation';
 import { motion } from 'framer-motion';
 import { useAnimationScroll } from '../../../hooks/useAnimationScroll';
 import useWindowDimensions from "../../../hooks/useWindowDimensions"
+import styles from "./About.module.css"
 
 export default function About() {
   const size = useWindowDimensions()
   const animation = useAnimationScroll(1)
 
   return (
-    <div className='w-100 h-100 align-center d-flex justify-between items-center'>
+    <div className={`${styles.container} align-center d-flex justify-between items-center`}>
       <div {...animation} className='w-5/12'>
         <div className='d-flex items-center w-100 flex-column mb-6'>
           <h1 className='mb-4 text-5xl font-bold'>About me</h1>
@@ -20,7 +21,7 @@ export default function About() {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis exercitationem labore architecto! Itaque, nesciunt obcaecati accusantium quo enim temporibus, nostrum praesentium consequuntur et sed provident impedit repellat reprehenderit iusto fuga.</p>
       </div>
       <div className='w-7/12 d-flex justify-center'>
-        <DesktopAnimation width={600} height={600}/>
+        <DesktopAnimation width={size.height * 0.70} height={size.height * 0.70}/>
       </div>
     </div>);
 }
