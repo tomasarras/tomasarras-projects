996e77b096c165948b27ebd64337e372f75e73c9
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-01-04 16:38:53 -0300

    chore: parallax decorations

--- components/Pages/Contact/Contact.js
+++ components/Pages/Contact/Contact.js
@@ -4,11 +4,70 @@ import { motion } from 'framer-motion';
 import { useAnimationScroll } from '../../../hooks/useAnimationScroll';
 import useWindowDimensions from "../../../hooks/useWindowDimensions"
 import styles from "./Contact.module.css"
+import Image from 'next/image';
 
 export default function Contact() {
   const size = useWindowDimensions()
   const animation = useAnimationScroll(1)
+  const [current, setCurrent] = useState(0)
+  const imgSize = 2159
+  const images = [
+    // "/portrait/dark/APC_0009_fade.png",
+    // "/portrait/dark/APC_0009_no_fade.png",
+    // "/portrait/dark/APC_0010_fade.png",
+    // "/portrait/dark/APC_0010_no_fade.png",
+    // "/portrait/dark/APC_0031_fade.png",
+    // "/portrait/dark/APC_0031_no_fade.png",
+    // "/portrait/dark/APC_0032_fade.png",
+    // "/portrait/dark/APC_0032_no_fade.png",
+    // "/portrait/dark/APC_0037_fade.png",
+    // "/portrait/dark/APC_0037_no_fade.png",
+    // "/portrait/dark/APC_0041_fade.png",
+    // "/portrait/dark/APC_0041_no_fade.png",
+    // "/portrait/dark/APC_0042_fade.png",
+    // "/portrait/dark/APC_0042_no_fade.png",
+    // "/portrait/dark/APC_0043_fade.png",
+    "/portrait/dark/APC_0043_no_fade.png",//
+    // "/portrait/dark/APC_0044_fade.png",
+    // "/portrait/dark/APC_0044_no_fade.png",
+    // "/portrait/dark/APC_0050_fade.png",
+    // "/portrait/dark/APC_0050_no_fade.png",
+    // "/portrait/dark/APC_0051_fade.png",
+    // "/portrait/dark/APC_0051_no_fade.png",
+    // "/portrait/dark/APC_0053_fade.png",
+    // "/portrait/dark/APC_0053_no_fade.png",
+    // "/portrait/dark/APC_0057_fade.png",
+    // "/portrait/dark/APC_0057_no_fade.png",
 
+    // "/portrait/lighted/APC_0059_fade_1.png",    
+    // "/portrait/lighted/APC_0059_fade_2.png",    
+    // "/portrait/lighted/APC_0059_no_fade.png",    
+    // "/portrait/lighted/APC_0064_fade_1.png",    
+    // "/portrait/lighted/APC_0064_fade_2.png",    
+    // "/portrait/lighted/APC_0064_no_fade.png",  
+    // "/portrait/lighted/APC_0068_fade_1.png",    
+    // "/portrait/lighted/APC_0068_fade_2.png",    
+    // "/portrait/lighted/APC_0068_fade_3.png",    
+    // "/portrait/lighted/APC_0068_no_fade.png", 
+    // "/portrait/lighted/APC_0059_fade_1.png",    
+    // "/portrait/lighted/APC_0059_fade_2.png",    
+    // "/portrait/lighted/APC_0059_no_fade.png",   
+    // "/portrait/lighted/APC_0059_no_fade_2.png",   
+    // "/portrait/lighted/APC_0070_fade_1.png",    
+    // "/portrait/lighted/APC_0070_fade_2.png",    
+    // "/portrait/lighted/APC_0070_no_fade.png", // ESTA
+    // "/portrait/lighted/APC_0073_fade_1.png",    
+    // "/portrait/lighted/APC_0073_fade_2.png",    
+    // "/portrait/lighted/APC_0073_no_fade.png", 
+    
+    // "/portrait/perfil/APC_0054_fade.png",    
+    // "/portrait/perfil/APC_0054_no_fade.png",    
+    
+    // "/portrait/perfillight/APC_0071_fade.png",    
+    // "/portrait/perfillight/APC_0071_no_fade.png",    
+    // "/portrait/perfillight/APC_0072.png",    //ESTA
+    
+  ]
   return (
     <div className={`container-100dvh align-center d-flex justify-between items-center`}>
       <div {...animation} className='w-6/12'>
@@ -19,7 +78,10 @@ You can also follow me on Instagram.
 tomasarras@gmail.com</p>
       </div>
       <div className='w-6/12 d-flex justify-center'>
-        Imagen
+        <div className={`${styles.imageContainer} flex justify-center items-center w-8/12`}>
+          {/* TODO: quitarle gradiente y ver si la imagen esta bien */}
+          <Image alt="Tomas Arras" onClick={ () => setCurrent(current+1)} /*style={{filter: "grayscale(100%)"}}*/ /*src={images[current]}*/ src="/portrait/contact.png" width={imgSize} height={imgSize}/>
+        </div>
       </div>
     </div>);
 }
--- components/Parallax/index.js
+++ components/Parallax/index.js
@@ -8,32 +8,59 @@ import { Context } from '../../Context';
 import { motion } from 'framer-motion';
 import { animationScrollDuration } from '../../constants';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
+import Image from 'next/image';
 
 export default function Parallax() {
   const { currentPage } = useContext(Context);
   const size = useWindowDimensions();
+  const tomasarrasTop = (size.height * 3) + (size.height / 1.65)
+  const lines3Top = (size.height * 2) + (size.height / 1.5)
+  const lines4Top = (size.height * 3) + (size.height / 1.5)
+  const lines5Top = (size.height * 4) + (size.height / 1.5)
   const animation = (translation) => (
     {
       animate: { y: currentPage*translation*-1 },
       transition: { ease:"circInOut", duration: animationScrollDuration/1000 }
-    })
+    }
+  )
+
+  const animationT = (translation, page) => {
+    const offset = currentPage * size.height *-1;
+    const offsetTranslation = currentPage - page
+    const y = offset + (offsetTranslation*translation*-1)
+    return {
+      animate: { y },
+      transition: { ease:"circInOut", duration: animationScrollDuration/1000 }
+    }
+  }
 
   return (<div className={styles.layer1}>
-    <motion.div {...animation(400)}  className={`${styles.lines1} ${currentPage >= 1 ? styles.lines1Passed : ''}`}>
+    <motion.div {...animation(size.height * .43)}  className={`${styles.lines1}`}>
       <CodeLines width={size.width * 0.25} height={size.width * 0.25}/>
     </motion.div>
-    <motion.div {...animation(900)} className={`${styles.lines2} ${currentPage >= 1 ? styles.lines2Passed : ''}`}>
+    <motion.div {...animation(size.height * .95)} className={`${styles.lines2}`}>
       <CodeLines width={size.width * 0.45} height={size.width * 0.45}/>
     </motion.div>
     <div className={`${styles.mouse} ${currentPage >= 1 ? styles.mousePassed : ''}`}>
       <MouseAnimation/>
     </div>
-    <motion.div {...animation(250)} className={`${styles.semicolon} ${currentPage >= 1 ? styles.semicolonPassed : ''}`}>
+    <motion.div {...animation(size.height * .266)} className={`${styles.semicolon}`}>
       <Semicolon width={size.width * 0.02} height={size.width * 0.02}/>
     </motion.div>
-    <motion.div {...animation(300)} className={`${styles.dev} ${currentPage >= 1 ? styles.devPassed : ''}`}>
+    <motion.div {...animation(size.height * .32)} className={`${styles.dev}`}>
       <Dev width={size.width * 0.07} height={size.width * 0.07}/>
     </motion.div>
-    
+    <motion.div {...animationT(size.height *.5, 3)} className={`${styles.tomasarras}`} style={{top: tomasarrasTop}}>
+      <Image src="/portrait/portrait_deco.png" width={size.width * 0.2} height={size.width * 0.2} alt='Tomas Arras'/>
+    </motion.div>
+    <motion.div {...animationT(size.height *.5, 2)}  className={`${styles.lines3}`} style={{top: lines3Top}}>
+      <CodeLines width={size.width * 0.25} height={size.width * 0.25}/>
+    </motion.div>
+    <motion.div {...animationT(size.height *.2, 3)}  className={`${styles.lines4}`} style={{top: lines4Top}}>
+      <CodeLines width={size.width * 0.25} height={size.width * 0.25}/>
+    </motion.div>
+    <motion.div {...animationT(size.height *.2, 4)}  className={`${styles.lines3}`} style={{top: lines5Top}}>
+      <CodeLines width={size.width * 0.25} height={size.width * 0.25}/>
+    </motion.div>
   </div>)
 }
