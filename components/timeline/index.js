2fc78d5829f9f74d016657a7bd146c518c0ceea4
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-01-15 13:06:15 -0300

    fix: experience list rendering

--- components/timeline/index.js
+++ components/timeline/index.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 const Badge = ({ children }) => (<span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">{children}</span>);
 
-export default function WorkTimeline({ }) {
+const Item = ({ time, children, title, i, first, last }) => {
   const fadeInAnimationVariants = {
     initial: {
       opacity: 0,
@@ -18,25 +18,26 @@ export default function WorkTimeline({ }) {
       }
     })
   }
+  return (
+  <li className={`${styles.item} ps-4 pe-4 rounded`}>
+    <div className={`${styles.itemContainer} ${last ? styles.lastContainer : ""} ${first ? styles.firstContainer : "pt-4"} ps-4 border-s border-gray-200 dark:border-gray-700`}>
+      <div className="absolute w-3 h-3 bg-gray-200 rounded-full start-4 ms-1 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+      <motion.div
+        variants={fadeInAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        custom={i}
+      >
+        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{time}</time>
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{title}</h3>
+        {children}
+      </motion.div>
+      {/* TODO normal debe ser text-gray-500 y dark 400 */}
+    </div>
+  </li>)
+}
 
-  const Item = ({ time, children, title, i, first, last }) => (
-    <li className={`${styles.item} ps-4 pe-4 rounded`}>
-      <div className={`${styles.itemContainer} ${last ? styles.lastContainer : ""} ${first ? styles.firstContainer : "pt-4"} ps-4 border-s border-gray-200 dark:border-gray-700`}>
-        <div className="absolute w-3 h-3 bg-gray-200 rounded-full start-4 ms-1 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <motion.div
-          variants={fadeInAnimationVariants}
-          initial="initial"
-          whileInView="animate"
-          custom={i}
-        >
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{time}</time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{title}</h3>
-          {children}
-        </motion.div>
-        {/* TODO normal debe ser text-gray-500 y dark 400 */}
-      </div>
-    </li>
-  )
+export default function WorkTimeline({ }) {
 
   return (
     <div>
@@ -46,7 +47,7 @@ export default function WorkTimeline({ }) {
       {/* TODO: university */}
       {/* TODO: badges icons */}
         <ol className="relative">                  
-          <Item i={2} time="February 2022 - 2024 (present)" title="Backend developer at CertiSur" first>
+          <Item i={0} time="February 2022 - 2024 (present)" title="Backend developer at CertiSur" first>
             <p className='text-gray-500 dark:text-gray-400 mb-3'>Development of a service with multiserveces and digital signature</p>
             <Badge>Java</Badge>
             <Badge>Spring</Badge>
@@ -58,7 +59,7 @@ export default function WorkTimeline({ }) {
             <Badge>Java</Badge>
             <Badge>Spring</Badge>
           </Item>
-          <Item i={0} time="January 2021 - September 2021" title="FullStack developer at IDEAAS" last><p className='text-gray-500 dark:text-gray-400 mb-3'>All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p></Item>
+          <Item i={2} time="January 2021 - September 2021" title="FullStack developer at IDEAAS" last><p className='text-gray-500 dark:text-gray-400 mb-3'>All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p></Item>
         </ol>
 
 
