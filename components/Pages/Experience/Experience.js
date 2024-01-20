00dd71afd6d2f69f4f8ac053889bf0be3ab4f9f6
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-01-20 15:39:03 -0300

    refactor: Timeline.js refactor file name

--- components/Pages/Experience/Experience.js
+++ components/Pages/Experience/Experience.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./Experience.module.css";
-import WorkTimeline from '../../timeline';
+import WorkTimeline from '../../timeline/Timeline';
 import { useAnimationScroll } from '../../../hooks/useAnimationScroll';
 import { motion } from 'framer-motion';
 import AnimationHandler from '../../Utils/AnimationHandler';
@@ -11,30 +11,29 @@ import experienceImageIllustration from "../../../public/experience-illustration
 
 export default function Experience({ innerRef }) {
   const size = useWindowDimensions()
-  console.log(size);
   const animation = useAnimationScroll(3);
 
   return (
   <div ref={innerRef} className='flex align-center justify-between items-center'>
-    <div className='flex flex-col xl:flex-row'>
+    <div className='grid grid-cols-1 lg:grid-cols-12 lg:gap-4 2xl:grid-cols-12'>
       <AnimationHandler
         isAnimationEnabled={isDesktop(size)}
         {...animation}
-        className='w-full xl:w-6/12 xl:me-2'>
+        className='xxl:flex xxl:items-center lg:col-span-4 xxl:col-span-6 2xl:col-span-4'>
         <div>
           <div className='d-flex items-center w-100 flex-column mb-6'>
             <h1 className='mb-4 text-5xl font-bold'>Experience</h1>
             <div className='title-underline'></div>
           </div>
           <p className='mb-10'>A lo largo de mi carrera, he tenido el privilegio de trabajar en diversos roles y tecnologías que han ampliado mi experiencia en el desarrollo web con tecnologías de frontend y de backend. Aquí hay un vistazo a algunas de mis experiencias más destacadas:</p>
-          <div className='hidden xl:flex justify-center'>
-            <div className='sm:w-8/12'>
+          <div className='hidden lg:flex justify-center'>
+            <div className='sm:w-8/12 lg:w-full xl:w-8/12'>
               <Image src={experienceImageIllustration}/>
             </div>
           </div>
         </div>
       </AnimationHandler>
-      <div className='xl:w-6/12 xl:ms-2'>
+      <div className='lg:col-span-8 xxl:col-span-6 2xl:col-start-6 2xl:col-span-7'>
         <WorkTimeline/>
       </div>
     </div>
