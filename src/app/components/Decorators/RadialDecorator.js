f5c187aa046a4fe9dfb3e2a98db03e42b912d741
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-02-03 15:28:52 -0300

    chore: changed RadialDecorator

--- src/app/components/Decorators/RadialDecorator.js
+++ src/app/components/Decorators/RadialDecorator.js
@@ -1,9 +1,8 @@
 import styles from "./RadialDecorator.module.css"
 
-export default function RadialDecorator({ top, left, size, halfSize }) {
+export default function RadialDecorator({ top, left = false, size }) {
 
   return (
-    /**TODO Check next 14 */
-    <div style={{top,left, width: size, height: size, background: `radial-gradient(circle ${halfSize}, var(--gradial-circle-radiants), var(--background-color) 100%)`}} className={`${styles["radial"]}`}></div>
+    <div style={{top,left: left ? "-15%" : "78%", width: "min(33vw, 640px)", height: size }} className={`${styles.radial} before:blur-2xl`}></div>
   );
 }
--- src/app/components/Decorators/RadialDecorators.js
+++ src/app/components/Decorators/RadialDecorators.js
@@ -2,16 +2,13 @@ import RadialDecorator from './RadialDecorator';
 
 export default function RadialDecorators() {
   const radialSize = "min(1920px, 100vw)"
-  const halfRadialSize = "min(960px, 50vw)"
-  const negativeHalfRadialSize = "max(-960px, -50vw)"
-  const thirdRadialSize = "min(1440px, 75vw)"
 
   return (
   <div className={"layer2"}>
-    <RadialDecorator top={`50vh`} left={`${negativeHalfRadialSize}`}  size={radialSize} halfSize={halfRadialSize}/>
-    <RadialDecorator top={`200vh`} left={`${halfRadialSize}`}  size={radialSize} halfSize={halfRadialSize}/>
-    <RadialDecorator top={`300vh`} left={`${negativeHalfRadialSize}`}  size={radialSize} halfSize={halfRadialSize}/>
-    <RadialDecorator top={`430vh`} left={`${thirdRadialSize}`}  size={radialSize} halfSize={halfRadialSize}/>
+    <RadialDecorator top={`50vh`}  size={radialSize} left/>
+    <RadialDecorator top={`200vh`} size={radialSize}/>
+    <RadialDecorator top={`300vh`} size={radialSize} left/>
+    <RadialDecorator top={`430vh`} size={radialSize}/>
   </div>
   )
 }
