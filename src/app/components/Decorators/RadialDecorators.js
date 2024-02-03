80617d9f45b2eb4d6d07ff2390ab72a22fc7a64a
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-02-03 16:09:00 -0300

    fix: responsive final space

--- src/app/components/Decorators/RadialDecorators.js
+++ src/app/components/Decorators/RadialDecorators.js
@@ -1,10 +1,21 @@
+"use client"
+import { useEffect, useRef, useState } from 'react';
 import RadialDecorator from './RadialDecorator';
 
 export default function RadialDecorators() {
+  const containerRef = useRef()
   const radialSize = "min(1920px, 100vw)"
+  const [parentHeight, setParentHeight] = useState("600vh")
+
+  useEffect(() => {
+    if (containerRef.current){ 
+      setParentHeight(containerRef.current.parentElement.offsetHeight)
+    }
+  }, [containerRef]);
+  
 
   return (
-  <div className={"layer2"}>
+  <div ref={containerRef} style={{height: parentHeight + "px"}} className={"layer2"}>
     <RadialDecorator top={`50vh`}  size={radialSize} left/>
     <RadialDecorator top={`200vh`} size={radialSize}/>
     <RadialDecorator top={`300vh`} size={radialSize} left/>
