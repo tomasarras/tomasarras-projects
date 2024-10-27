3d78cf31aa7214ee1045e628b366418755e02a06
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-10-27 20:17:31 -0300

    chore: auth page template

---
+++ pages/auth/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import lockImage from '../../public/lock.jpg'
+import Image from 'next/image'
+
+export default function Auth() { 
+  
+  return <div><Image src={lockImage}/></div>
+}
