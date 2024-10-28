91d9c3f1b1222300764e25214e0cbb7ac7a30623
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-10-28 18:37:00 -0300

    chore: auth page

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import lockImage from '../../public/lock.jpg'
+import React, { useState } from 'react'
+import lockImage from '../../public/lock.png'
 import Image from 'next/image'
+import s from './auth.module.css'
 
 export default function Auth() { 
+  const [password, setPassword] = useState('')
+
+  const handleOnKeyDown = async e => {
+    if (e.key === 'Enter') {
+      const body = { password }
+      const url = process.env.NEXT_PUBLIC_AUTH_SERVER
+      const response = await fetch(`${url}/api/auth`, {
+        method: 'POST',
+        body: JSON.stringify(body)
+      })
+      console.log(response);
+    }
+  }
   
-  return <div><Image src={lockImage}/></div>
+  return <div className={`${s.center} ${s.flex} ${s.screen}`}>
+    <div className={s.logoAndInput}>
+      <div className={s.centerX}>
+        <Image className={s.logo}  src={lockImage} alt='lock'/>
+      </div>
+      <input type='password' className={s.input} value={password} onKeyDown={handleOnKeyDown} onChange={e => setPassword(e.target.value)} />
+    </div>
+  </div>
 }
